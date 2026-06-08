2b7732d3d976c09dde92e95de8b8e656a3a56bca
Luis Henrique <luis@prover.net.br>
2026-06-08 14:25:45 -0300

    first commit

--- src/main/java/com/example/restauranteback/service/PedidoService.java
+++ src/main/java/com/example/restauranteback/service/PedidoService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -60,5 +61,9 @@ public class PedidoService {
         pedido.setValorTotal(valorTotal);
 
         return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidoRepository.findAll();
     }
 }
